53272a892bd60de3fc0a319e3bcb2099051fe999
Antoine Eudes <antoine@eudes.co>
2018-03-01 21:34:03 +0100

    list of vehicles

--- tools.py
+++ tools.py
@@ -1,2 +1,13 @@
+from objects import Vehicle
+
 def norme1(intersection1, intersection2):
-    return abs(intersection1[0] - intersection2[0]) + abs(intersection1[1] - intersection2[1])
+    return abs(intersection1[0] - intersection2[0]) + abs(intesection1[1] - intersection2[1])
+
+def initializeVehicleList(grid):
+    n = grid.vehicleNumber
+     VehicleList = []
+     for i in range(n):
+         v = Vehicle()
+         VehicleList.append(v)
+
+    return VehicleList
