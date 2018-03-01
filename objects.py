0957eb8c9c65bafe48e0682a85ce41b25584c0f5
Antoine Eudes <antoine@eudes.co>
2018-03-01 20:08:49 +0100

    class

--- objects.py
+++ objects.py
@@ -1,1 +1,18 @@
+class Grid:
+    rows
+    columns
+    vehicleNumber
+    rideNumber
+    bonus
+    timeLimit
 
+class Vehicle:
+    
+
+
+class Ride:
+    startIntersection
+    finishIntersection
+    earliestStart
+    latestFinish
+    vehicle=false
